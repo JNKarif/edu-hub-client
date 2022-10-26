cc5fe38a952beff300845b18112edcf60c86027e
JNKarif <arif.rahman041@gmail.com>
2022-10-26 20:34:48 +0600

    aside-nav-added

--- src/Pages/Shared/AsideNav/AsideNav.js
+++ src/Pages/Shared/AsideNav/AsideNav.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const AsideNav = () => {
+    const [category, setCategory] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/category')
+            .then(res => res.json())
+            .then(data => setCategory(data))
+    }, [])
     return (
         <div>
-        <h2>Aside Nav</h2>    
+            <h2>Course List{category.length}</h2>
+            <div>
+{
+    category.map(course=><p key={course.id}>
+        <Link to={`/category/${course.id}`}>{course.name}</Link> 
+    </p>)
+}
+            </div>
         </div>
     );
 };
